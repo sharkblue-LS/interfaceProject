9c5a14a68f94674695cc5a027f378bd90543127c
sharkblue-LS <317318151@qq.com>
2021-08-09 18:27:34 +0800

    add_event_test

---
+++ TestReport-master/test_unittest_TestReport.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3 
+# coding=utf-8
+
+
+
+import unittest
+from TestReport import HTMLTestReport
+from TestReport import HTMLTestReportEN
+
+class Case_assert_1(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_ok(self):
+        self.assertEqual(1,1)
+
+    def test_faile(self):
+        self.assertEqual(1,2)
+
+    def test_error(self):
+        raise Exception
+
+class Case_assert_2(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_ok(self):
+        self.assertTrue(True)
+
+class Case_assert_3(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_error(self):
+        raise Exception
+
+class Case_assert_4(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def test_faile(self):
+        self.assertEqual(1,3)
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(Case_assert_1('test_ok'))
+    suite.addTest(Case_assert_1('test_faile'))
+    suite.addTest(Case_assert_1('test_error'))
+    suite.addTest(Case_assert_2('test_ok'))
+    suite.addTest(Case_assert_3('test_error'))
+    suite.addTest(Case_assert_4('test_faile'))
+
+    with open('./TestReport.html', 'wb') as report:
+        runner = HTMLTestReportEN(stream=report,
+                                verbosity=2,
+                                title='HwTestReport 测试',
+                                description='带截图，带饼图，带详情',
+                                tester='Johnny')
+        runner.run(suite)
