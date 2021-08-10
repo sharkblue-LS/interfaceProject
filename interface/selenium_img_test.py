6ed391cc58d49efd43850310a9d26eb7941a389a
sharkblue-LS <317318151@qq.com>
2021-08-10 15:38:27 +0800

    add_event_test

--- interface/selenium_img_test.py
+++ interface/selenium_img_test.py
@@ -1,22 +1,5 @@
-#!/usr/bin/env python3 
-# coding=utf-8
-__author__ = "zhaohongwei"
-__email__ = "hongweifuture@163.com"
-__contact__ = "https://blog.csdn.net/z_johnny"
-__version__ = "0.1"
-__date__ = "2019/9/9 14:26"
-__maintainer__ = "zhaohongwei,"
-__description__ = "unittest "
-
-"""
-History:
-2020/1/13 11:47 : python使用geckodriver驱动Firefox https://github.com/mozilla/geckodriver/releases
-2019/9/9 14:26 : Created by zhaohongwei 
-"""
-
+import sys
 import unittest
-from HwTestReport import HTMLTestReport
-from HwTestReport import HTMLTestReportEN
 from selenium import webdriver
 
 class Case_baidu(unittest.TestCase):
@@ -26,7 +9,7 @@ class Case_baidu(unittest.TestCase):
     '''
     def setUp(self):
         self.imgs = []
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
     def tearDown(self):
         self.driver.quit()
@@ -47,7 +30,7 @@ class Case_baidu(unittest.TestCase):
     def test_baidu_assert_ok(self):
         '''用例通过，有报告内容，有截图'''
         self.driver.get("https://www.baidu.com")
-        hao123 = self.driver.find_element_by_xpath('//*[@id="u1"]/a[2]').text
+        hao123 = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[2]').text
         print(hao123)
         self.get_screenshot()
         self.assertEqual(hao123, 'hao123')
@@ -55,7 +38,7 @@ class Case_baidu(unittest.TestCase):
     def test_baidu_assert_ok_noimg(self):
         '''用例通过，有报告内容，没有截图'''
         self.driver.get("https://www.baidu.com")
-        news = self.driver.find_element_by_xpath('//*[@id="u1"]/a[1]').text
+        news = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[1]').text
         print(news)
         self.assertEqual(news, u"新闻")
 
@@ -63,10 +46,10 @@ class Case_baidu(unittest.TestCase):
         '''用例失败，带有失败内容和截图'''
         self.driver.get("https://www.baidu.com")
         self.get_screenshot()
-        news = self.driver.find_element_by_xpath('//*[@id="u1"]/a[1]').text
+        news = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[1]').text
         print(news)
         self.get_screenshot()
-        self.driver.find_element_by_xpath('//*[@id="u1"]/a[1]').click()
+        self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[2]').click()
         self.get_screenshot()
         self.assertEqual(news, 'hao123')
 
@@ -83,7 +66,7 @@ class Case_qq(unittest.TestCase):
     '''
     def setUp(self):
         self.imgs = []
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
     def tearDown(self):
         self.driver.quit()
@@ -105,7 +88,7 @@ class Case_qq(unittest.TestCase):
 class Case_163(unittest.TestCase):
     def setUp(self):
         self.imgs = []
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
     def tearDown(self):
         self.driver.quit()
@@ -113,21 +96,3 @@ class Case_163(unittest.TestCase):
     def test_163_ok(self):
         '''通过 没有内容和截图'''
         self.driver.get("https://www.163.com/")
-
-
-if __name__ == "__main__":
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(Case_baidu)
-    suite2 = unittest.TestLoader().loadTestsFromTestCase(Case_qq)
-    suite3 = unittest.TestLoader().loadTestsFromTestCase(Case_163)
-    suites = unittest.TestSuite()
-    suites.addTests([suite1, suite2, suite3])
-
-    # HTMLTestReport or HTMLTestReportEN
-    with open('./HwTestReportIMG.html', 'wb') as report:
-        runner = HTMLTestReportEN(stream=report,
-                                verbosity=2,
-                                images=True,
-                                title='HwTestReport 测试',
-                                description='带截图，带饼图，带详情',
-                                tester='Johnny')
-        runner.run(suites)
--- run_tests.py
+++ run_tests.py
@@ -4,33 +4,49 @@ sys.path.append('./db_fixture')
 sys.path.append('./TestReport-master/Local')
 from db_fixture import test_data
 from interface import add_event_test
-from TestReport_local import HTMLTestReport
+from interface import selenium_img_test
+from TestReport_local import HTMLTestReport,HTMLTestReportEN
+
 
 #指定测试用例为当前文件夹下的interface目录
 test_dir = './interface'
 discover = unittest.defaultTestLoader.discover(test_dir,pattern='*_test.py')
 
+# if __name__ == '__main__':
+#     test_data.init_data()
+#     suite = unittest.TestSuite()
+#     suite.addTest(add_event_test.AddEventTest('test_add_event_all_null'))
+#     suite.addTest(add_event_test.AddEventTest('test_add_event_eid_exist'))
+#     suite.addTest(add_event_test.AddEventTest('test_add_event_name_exist'))
+#     suite.addTest(add_event_test.AddEventTest('test_add_event_data_type_error'))
+#     suite.addTest(add_event_test.AddEventTest('test_add_event_success'))
+#
+#     with open('./report/TestReport.html', 'wb') as report:
+#         runner = HTMLTestReport(stream=report,
+#                                 verbosity=2,
+#                                 title='TestReport 测试',
+#                                 description='带截图，带饼图，带详情',
+#                                 tester='shark')
+#         runner.run(suite)
+
 if __name__ == '__main__':
-    test_data.init_data()
-    suite = unittest.TestSuite()
-    suite.addTest(add_event_test.AddEventTest('test_add_event_all_null'))
-    suite.addTest(add_event_test.AddEventTest('test_add_event_eid_exist'))
-    suite.addTest(add_event_test.AddEventTest('test_add_event_name_exist'))
-    suite.addTest(add_event_test.AddEventTest('test_add_event_data_type_error'))
-    suite.addTest(add_event_test.AddEventTest('test_add_event_success'))
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(selenium_img_test.Case_baidu)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(selenium_img_test.Case_qq)
+    suite3 = unittest.TestLoader().loadTestsFromTestCase(selenium_img_test.Case_163)
+    suites = unittest.TestSuite()
+    # suites.addTest(selenium_img_test.Case_baidu('test_baidu_search'))
+    # suites.addTest(selenium_img_test.Case_baidu('test_baidu_assert_ok'))
     
-    with open('./report/TestReport.html', 'wb') as report:
+    suites.addTests([suite1, suite2, suite3])
+    
+    # HTMLTestReport or HTMLTestReportEN
+    with open('./report/TestReportIMG.html', 'wb') as report:
         runner = HTMLTestReport(stream=report,
                                 verbosity=2,
+                                images=True,
                                 title='TestReport 测试',
                                 description='带截图，带饼图，带详情',
                                 tester='shark')
-        runner.run(suite)
+        runner.run(suites)
     
-    # now = time.strftime('%Y-%m-%d %H:%M:%S')
-    # filename = './report/'+ now + '_result.html'
-    # fp = open(filename,'wb')
-    # runner = HTMLTestRunner(stream=fp,title='Guest Manage System interface Test Report',description='Implementation Example with:')
-    # runner.run(discover)
-    # fp.close()
     
