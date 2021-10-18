363bb0c67225e5e30b23451ff37c1dc0f7dd6840
sharkblue-LS <317318151@qq.com>
2021-10-18 18:46:05 +0800

    add_event_test

--- interface/selenium_img_test.py
+++ interface/selenium_img_test.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class Case_baidu(unittest.TestCase):
     '''
@@ -22,15 +23,15 @@ class Case_baidu(unittest.TestCase):
         '''用例通过，没有报告内容，有多张截图'''
         self.driver.get("https://www.baidu.com")
         self.get_screenshot()
-        self.driver.find_element_by_id('kw').send_keys(u'百度一下')
+        self.driver.find_element(By.ID,'kw').send_keys(u'百度一下')
         self.get_screenshot()
-        self.driver.find_element_by_id('su').click()
+        self.driver.find_element(By.ID,'su').click()
         self.get_screenshot()
 
     def test_baidu_assert_ok(self):
         '''用例通过，有报告内容，有截图'''
         self.driver.get("https://www.baidu.com")
-        hao123 = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[2]').text
+        hao123 = self.driver.find_element(By.XPATH,'//*[@id="s-top-left"]/a[2]').text
         print(hao123)
         self.get_screenshot()
         self.assertEqual(hao123, 'hao123')
@@ -38,7 +39,7 @@ class Case_baidu(unittest.TestCase):
     def test_baidu_assert_ok_noimg(self):
         '''用例通过，有报告内容，没有截图'''
         self.driver.get("https://www.baidu.com")
-        news = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[1]').text
+        news = self.driver.find_element(By.XPATH,'//*[@id="s-top-left"]/a[1]').text
         print(news)
         self.assertEqual(news, u"新闻")
 
@@ -46,10 +47,10 @@ class Case_baidu(unittest.TestCase):
         '''用例失败，带有失败内容和截图'''
         self.driver.get("https://www.baidu.com")
         self.get_screenshot()
-        news = self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[1]').text
+        news = self.driver.find_element(By.XPATH,'//*[@id="s-top-left"]/a[1]').text
         print(news)
         self.get_screenshot()
-        self.driver.find_element_by_xpath('//*[@id="s-top-left"]/a[2]').click()
+        self.driver.find_element(By.XPATH,'//*[@id="s-top-left"]/a[2]').click()
         self.get_screenshot()
         self.assertEqual(news, 'hao123')
 
@@ -74,16 +75,16 @@ class Case_qq(unittest.TestCase):
     def test_qq_index_faile(self):
         '''用例错误，带有错误内容和没有截图'''
         self.driver.get("https://www.qq.com")
-        self.driver.find_element_by_id('sougouTxt').send_keys(u'搜狗搜索')
+        self.driver.find_element(By.ID,'sougouTxt').send_keys(u'搜狗搜索')
         # self.driver.find_element_by_id('sougouTxt').send_keys(u'搜狗搜索')
-        self.driver.find_element_by_id('searchBtn').click()
+        self.driver.find_element(By.ID,'searchBtn').click()
         self.assertIn(u"搜狗", u'搜索')
 
     def test_qq_index_ok(self):
         '''用例通过，没有内容和没有截图'''
         self.driver.get("https://www.qq.com")
-        self.driver.find_element_by_id('sougouTxt').send_keys(u'搜狗搜索')
-        self.driver.find_element_by_id('searchBtn').click()
+        self.driver.find_element(By.ID,'sougouTxt').send_keys(u'搜狗搜索')
+        self.driver.find_element(By.ID,'searchBtn').click()
 
 class Case_163(unittest.TestCase):
     def setUp(self):
